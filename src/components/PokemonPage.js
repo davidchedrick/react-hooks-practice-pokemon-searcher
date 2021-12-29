e3da1d82bc22118f62e23e09c73215064ae567ba
David <davidchedrick@gmail.com>
2021-12-28 18:06:43 -0600

    commit tree 99c3d383564d6fa8638a2e3ca38933a70c842f78

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+
+  const [pokemon, setPokemon] = useState([])
+  const [searchTerm, setSearchTerm]= useState("")
+  console.log("pokemon: ", pokemon);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/pokemon")
+    .then(resp => resp.json())
+    .then(setPokemon)
+  }, [])
+
+  const pokemonToDisplay = pokemon.filter(poke => poke.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
+  function handleAddPokemon(newPokemon) {
+    setPokemon([newPokemon, ...pokemon]);
+  }
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm onAddPokemon={handleAddPokemon} />
       <br />
-      <Search />
+      <Search 
+        onChangeSearch={setSearchTerm} 
+        searchTerm={searchTerm}
+      />
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemon={pokemonToDisplay} />
     </Container>
   );
+
 }
 
 export default PokemonPage;
